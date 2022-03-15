c64aca0fb71af7105196ba5d1464d798df72c187
Nikita Mehtre <mehtrenikita5@gmail.com>
2022-03-16 00:39:10 +0530

    Add login templets

---
+++ src/main/java/com/example/springBootApp/contoller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.example.springBootApp.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+	@GetMapping("/login")
+	public String Login() {
+		return "login";
+	}
+
+	@PostMapping("/login-submit")
+	public String submitLogin(@RequestParam String email, @RequestParam String password) {
+		System.out.println(email + password);
+		return "login";
+	}
+
+	@GetMapping("/register")
+	public String Register() {
+		return "register";
+	}
+
+	@PostMapping("/register-submit")
+	public void submitRegister() {
+		System.out.println("SubmitRegistration");
+	}
+
+
+
+}
